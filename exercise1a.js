9b7ea37ec4627c8abe4ac92808e10ad3426ba50f
CODERGICHUHI <87018887+CODERGICHUHI@users.noreply.github.com>
2022-02-14 13:01:07 +0300

    code for creating a loop triangle

--- exercise1a.js
+++ exercise1a.js
@@ -1,1 +1,6 @@
+//In this exercises we are supposed to create a rectangle with seven # using a loop 
+// solution
+for (let line = '#'; line.length < 8; line += '#') {
+        console.log(line)
+}
 
